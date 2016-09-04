d6f378e4672a1b5a13442df2193355872d2efb6f
zivatar <macgyver1024@gmail.com>
2016-09-04 23:59:12 +0200

    [m] Animated hamburger icon

---
+++ module3_solution/js/script.js
@@ -0,0 +1,6 @@
+// ANIMATED HAMBURGER ICON https://codepen.io/impondesk/pen/EaKoXY
+$(document).ready(function () {
+			  $(".navbar-toggle").on("click", function () {
+				    $(this).toggleClass("active");
+			  });
+		});
